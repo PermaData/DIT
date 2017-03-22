4356c2eae9c014c1e5cbb085c3603b0ae239d122
Hannah Wilcox <hwilcox@vpn-nsidc222-dhcp.int.colorado.edu>
2017-03-22 19:29:47 -0400

    Remove rill from dit_flow.

--- dit_flow/setup.py
+++ dit_flow/setup.py
@@ -2,10 +2,6 @@ import os
 from distutils.core import run_setup
 from setuptools import setup, find_packages
 
-os.chdir('./rill')
-run_setup('setup.py', ['install'])
-os.chdir('..')
-
 setup(name='dit_widget',
       version='0.0.0.dev1',
       description='Data Integration Tool (DIT) designed to help debug data transformation.',
